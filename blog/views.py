a242955db4e08bdb40218816465528e9cf71d76b
Miss Nadia <gbqlaced@gmail.com>
2025-03-05 15:39:04 +0000

    Fix indentation issues in comment submission logic in post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404 
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib import messages
 from .models import Post
@@ -29,19 +29,20 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    
     if request.method == "POST":
-    comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-        messages.add_message(
-        request, messages.SUCCESS,
-        'Comment submitted and awaiting approval'
-    )
-        
-    comment_form = CommentForm()  
+        comment_form = CommentForm(data=request.POST)  # Fixed indentation
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)  # Fixed indentation
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    else:
+        comment_form = CommentForm()  # Fixed indentation
 
     return render(
         request,
@@ -52,4 +53,4 @@ def post_detail(request, slug):
             "comment_count": comment_count,
             "comment_form": comment_form,
         },
-    )    
+    )
