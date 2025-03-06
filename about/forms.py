5be7284f1555b677123a9549c1db003583dc33a2
Miss Nadia <gbqlaced@gmail.com>
2025-03-06 10:13:47 +0000

    Update CollaborateForm to use absolute import and change fields format

--- about/forms.py
+++ about/forms.py
@@ -1,7 +1,8 @@
+from .models import CollaborateRequest
 from django import forms
-from blog.models import CollaborateRequest  # Use absolute import
+
 
 class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
-        fields = ['name', 'email', 'message']
+        fields = ('name', 'email', 'message')
